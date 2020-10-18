582e7a2b3875a66e8834a16a5352b7f93f04225c
nishkohli96 <nishkohli96@gmail.com>
2020-10-19 02:24:54 +0530

    bank srch page done

--- app/scenes/SearchBank.js
+++ app/scenes/SearchBank.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { TextInput, Snackbar } from 'react-native-paper';
 import ScreenHeader from '@atoms/ScreenHeader';
 import GetBankDetails from '@services/GetBankDetails';
 import { useNavigation } from 'react-navigation-hooks';
-import {useNetInfo} from "@react-native-community/netinfo";
+import { useNetInfo} from "@react-native-community/netinfo";
 
 const SearchBank = () => {
   const navigation = useNavigation();
   const netInfo = useNetInfo();
   const [text, setText] = React.useState(null);
+  
+  const [visible, setVisible] = React.useState(false);
+  const [sbText, setsbText] = React.useState(null);
+  const onDismissSnackBar = () => setVisible(false);
 
 	const getBankDetails = async () => {
     if(text === null){
-      console.log('null text')
-      return false;
+      setVisible(true);
+      setsbText('Please enter something....')
     }
     else if (!netInfo.isConnected){
-      console.log('no net')
-      return false;
+      setVisible(true);
+      setsbText('No Internet Connection Detected')
     } 
     else {
 		  const result = await GetBankDetails(text.trim());
@@ -28,12 +32,14 @@ const SearchBank = () => {
       } 
       else 
       {
+        setVisible(true);
+        setsbText('No Bank Info Found')
       }
     }
 	};
 
 	return (
-		<View>
+		<View style={styles.container}>
 			<ScreenHeader title="Search Bank" />
 			<View style={styles.body}>
 				<TextInput
@@ -52,11 +58,19 @@ const SearchBank = () => {
 					/>
 				</View>
 			</View>
+
+      <Snackbar visible={visible} duration={2000} style={styles.snackBar} onDismiss={() => setVisible(false)}>
+        {sbText}
+      </Snackbar>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
+  container:{
+    display: 'flex',
+    flex: 1
+  },
 	body: {
 		padding: 30
 	},
@@ -65,7 +79,16 @@ const styles = StyleSheet.create({
 	},
 	srchBtn: {
 		marginTop: 30
-	}
+  },
+  snackBar: {
+    position: 'absolute',
+    bottom: 50,
+    backgroundColor: '#595959',
+    opacity: 0.7,
+    borderRadius: 10,
+    marginLeft: 30,
+    marginRight: 30
+  }
 });
 
 export default SearchBank;
